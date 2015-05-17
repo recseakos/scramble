65cfd4672f87440d6f82d2cd6a5ff639a8a5f340
RcsAkos <recseakos@gmail.com>
2015-05-17 22:57:03 +0200

    classes

---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {	
+		TestCase tests = new TestCase();
+		tests.testWordScrambler();
+		tests.testTextScrambler();
+	}
+}
